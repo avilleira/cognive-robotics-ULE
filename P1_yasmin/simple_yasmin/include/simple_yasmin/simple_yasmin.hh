da474639d82f96c472d70da47e4d07704f49b7df
Jaime Avileira <j.avilleirapriv@gmail.com>
2026-03-13 01:12:37 +0100

    creating repository and P1 [P1-YASMIN]
    Signed-off-by: Jaime Avileira <j.avilleirapriv@gmail.com>

---
+++ P1_yasmin/simple_yasmin/include/simple_yasmin/simple_yasmin.hh
@@ -0,0 +1,10 @@
+#include <chrono>
+#include <iostream>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "yasmin/logs.hpp"
+#include "yasmin/state.hpp"
+#include "yasmin/state_machine.hpp"
+#include "yasmin_ros/ros_logs.hpp"
+#include "yasmin_viewer/yasmin_viewer_pub.hpp"
