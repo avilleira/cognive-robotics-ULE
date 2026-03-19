a40fdbcb63bf475cc707c20a0655e595064907e3
Jaime Avileira <j.avilleirapriv@gmail.com>
2026-03-19 01:23:11 +0100

    feat(simple_yasmin): Creating the class for the basic yasmin node [P1_yasmin]
    Signed-off-by: Jaime Avileira <j.avilleirapriv@gmail.com>

--- P1_yasmin/simple_yasmin/include/simple_yasmin/simple_yasmin.hh
+++ P1_yasmin/simple_yasmin/include/simple_yasmin/simple_yasmin.hh
@@ -8,3 +8,60 @@
 #include "yasmin/state_machine.hpp"
 #include "yasmin_ros/ros_logs.hpp"
 #include "yasmin_viewer/yasmin_viewer_pub.hpp"
+
+
+/**
+ * @brief Represents the "Foo" state in the state machine.
+ *
+ * This state increments a counter each time it is executed and
+ * communicates the current count via the blackboard.
+ */
+class FooState : public yasmin::State
+{
+public:
+    int counter;
+
+    /**
+    * @brief Constructs a FooState object, initializing the counter.
+    */
+    FooState();
+
+    /**
+    * @brief Executes the Foo state logic.
+    *
+    * This method logs the execution, waits for 3 seconds,
+    * increments the counter, and sets a string in the blackboard.
+    * The state will transition to either "outcome1" or "outcome2"
+    * based on the current value of the counter.
+    *
+    * @param blackboard Shared pointer to the blackboard for state communication.
+    * @return std::string The outcome of the execution: "outcome1" or "outcome2".
+    */
+    std::string execute(yasmin::Blackboard::SharedPtr blackboard) override;
+};
+
+
+/**
+* @brief Represents the "Bar" state in the state machine.
+*
+* This state logs the value from the blackboard and provides
+* a single outcome to transition.
+*/
+class BarState : public yasmin::State
+{
+    /**
+    * @brief Constructs a BarState object.
+    */
+    BarState();
+
+    /**
+    * @brief Executes the Bar state logic.
+    *
+    * This method logs the execution, waits for 3 seconds,
+    * retrieves a string from the blackboard, and logs it.
+    *
+    * @param blackboard Shared pointer to the blackboard for state communication.
+    * @return std::string The outcome of the execution: "outcome3".
+    */
+    std::string execute(yasmin::Blackboard::SharedPtr blackboard) override;
+};
---
+++ P1_yasmin/simple_yasmin/src/simple_yasmin.cc
@@ -0,0 +1,43 @@
+#include "simple_yasmin.hh"
+
+
+// FOOState Class
+FooState::FooState()
+: yasmin::State({"outcome1", "outcome2"}), counter {0}
+{
+
+}
+
+
+std::string FooState::execute(yasmin::Blackboard::SharedPtr blackboard)
+{
+    YASMIN_LOG_INFO("Ececuting state FOO");
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+
+    if (this->counter < 3) {
+        this->counter++;
+        blackboard->set<std::string>("foo_str", "Counter: " + std::to_string(this->counter));
+        return "outcome1";
+    } else {
+        return "outcome2";
+    }
+}
+
+
+//BarState CLass
+BarState::BarState()
+: yasmin::State({"outcome3"})
+{
+
+}
+
+
+std::string BarState::execute(yasmin::Blackboard::SharedPtr blackboard)
+{
+    YASMIN_LOG_INFO("Executing state BAR");
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+
+    YASMIN_LOG_INFO(blackboard->get<std::string>("foo_str").c_str());
+
+    return "outcome3";
+}
